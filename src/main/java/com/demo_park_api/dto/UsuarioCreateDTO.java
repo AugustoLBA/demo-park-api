d823fbd793f3130f8eb112322cd46f3dc2f88680
Augusto LBA <j.augusto.lba@gmail.com>
2024-06-26 14:47:03 -0300

    adicionando a classe UsuarioCreateDTO

---
+++ src/main/java/com/demo_park_api/dto/UsuarioCreateDTO.java
@@ -0,0 +1,17 @@
+package com.demo_park_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioCreateDTO {
+
+    private String username;
+
+    private String password;
+}
