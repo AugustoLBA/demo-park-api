9f751ba8d222c8c66c9f119422556abd45d02439
Augusto LBA <j.augusto.lba@gmail.com>
2024-06-26 12:01:59 -0300

    adicionando o metodo salvar na classe UsuarioService

--- src/main/java/com/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/demo_park_api/service/UsuarioService.java
@@ -1,11 +1,18 @@
 package com.demo_park_api.service;
 
+import com.demo_park_api.entity.Usuario;
 import com.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @RequiredArgsConstructor // Injeção de depêndencia via lombok
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
 }
