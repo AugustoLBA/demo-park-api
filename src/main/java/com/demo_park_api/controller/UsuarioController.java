0b199877fde1f843376a40fe05bbd82a1306ed08
Augusto LBA <j.augusto.lba@gmail.com>
2024-06-26 12:06:47 -0300

    adicionando o metodo create na classe UsuarioController

--- src/main/java/com/demo_park_api/controller/UsuarioController.java
+++ src/main/java/com/demo_park_api/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.demo_park_api.controller;
 
+import com.demo_park_api.entity.Usuario;
 import com.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor // Injeção de depêndencia via lombok
@@ -10,4 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
